6c535156939b2f17a0a22a3ec89e4f699d504d07
milo <mrangelmilo@gmail.com>
2024-05-14 18:59:53 +0200

    Correction and adding problems' list

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import player
 pygame.init()
 
 sc_size = 1200, 600
-sc_center = sc_size[0]/2, sc_size[1]/2
+sc_center = sc_size[0] / 2, sc_size[1] / 2
 sc = pygame.display.set_mode(sc_size)
 pygame.display.set_caption("ça va b ?")
+sc.fill(pygame.Color(0, 0, 0))
 
-sc.fill(pygame.Color(0,0,0))
 
 def main():
-    players =[player.Player(sc), player.Player(sc, (254, 50, 254), pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN)]#, player.Player(sc, (50, 254, 50), pygame.K_b, pygame.K_n, pygame.K_h)]
+    player.Player(sc)
+    player.Player(sc, (254, 50, 254), pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN)
 
     clock = pygame.time.Clock()
 
@@ -20,14 +21,12 @@ def main():
         clock.tick(60)
 
         keys = pygame.key.get_pressed()
-        for p in players:
+        for p in player.Player.instances:
             p.jump(keys)
             if p.control(keys) or p.wall_collisions():
                 running = False
                 break
-
             p.draw()
-
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -35,7 +34,7 @@ def main():
 
         pygame.display.flip()
 
-
     pygame.quit()
+
 
 main()
--- player.py
+++ player.py
@@ -3,15 +3,17 @@ import math
 import pygame.time
 from random import randint
 
+
 class Player(pygame.sprite.Sprite):
     pre_positions = []
     positions = []
-    instance_len = 0
+    instances = []
     spawn_rect = 100
-    def __init__(self, surface, color=(0, 0, 254), k_left = pygame.K_q,  k_right = pygame.K_d, k_boost = pygame.K_z, k_speedup = pygame.K_s):
+
+    def __init__(self, surface, color=(0, 0, 254), k_left=pygame.K_q,  k_right=pygame.K_d, k_boost=pygame.K_z, k_speedup=pygame.K_s):
         super().__init__()
-        Player.instance_len += 1
-        self.instance_number = Player.instance_len
+        Player.instances.append(self)
+        self.instance_number = len(Player.instances)
         print('Player instance:', self.instance_number, '-> init...' )
 
         self.surface = surface
@@ -19,12 +21,11 @@ class Player(pygame.sprite.Sprite):
         self.x = randint(Player.spawn_rect, self.surface.get_size()[0] - Player.spawn_rect)
         self.y = randint(Player.spawn_rect, self.surface.get_size()[1] - Player.spawn_rect)
 
-        self.x_colision = self.x
-        self.y_colision = self.y
+        self.x_collision = self.x
+        self.y_collision = self.y
 
-
-        self.speed = self.speedup = 1
-        self.angle = self.spawn_angle = randint(0,359)
+        self.pixel_nb_for_move = self.speedup = 1
+        self.angle = randint(0, 359)
         self.angle_force = 0.04
 
         self.k_left = k_left
@@ -55,33 +56,30 @@ class Player(pygame.sprite.Sprite):
             self.last_speedup_time = pygame.time.get_ticks()
             self.speedup = 2
 
-
-
         if keys[self.k_left]:
             self.angle -= self.angle_force
         if keys[self.k_right]:
             self.angle += self.angle_force
 
         for _ in range(self.speedup):
-            dx = self.speed * math.cos(self.angle)
-            dy = self.speed * math.sin(self.angle)
+            dx = self.pixel_nb_for_move * math.cos(self.angle)
+            dy = self.pixel_nb_for_move * math.sin(self.angle)
 
             self.x += dx
             self.y += dy
 
-            self.x_colision = round(self.x, 0)
-            self.y_colision = round(self.y, 0)
+            self.x_collision = round(self.x, 0)
+            self.y_collision = round(self.y, 0)
             if self.jumping:
                 self.jump_positions.append((self.x, self.y))
             else:
-                if any((self.x_colision, self.y_colision) == pos for pos in Player.positions):
+                if any((self.x_collision, self.y_collision) == pos for pos in Player.positions):
                     return True
                 else:
-                    Player.pre_positions.append((self.x_colision, self.y_colision))
+                    Player.pre_positions.append((self.x_collision, self.y_collision))
                     if len(Player.pre_positions) > 10:
                         Player.positions.append(Player.pre_positions[0])
                         Player.pre_positions.remove(Player.pre_positions[0])
-
 
     def wall_collisions(self):
         if self.x < 0 or self.y < 0:
@@ -93,7 +91,8 @@ class Player(pygame.sprite.Sprite):
         if pygame.time.get_ticks() - self.last_boost_time >= self.boost_duration:
             self.jumping = False
         if not self.jumping and self.jump_positions != []:
-            pygame.draw.circle(self.surface, pygame.Color(0, 0, 0), (int(self.jump_positions[0][0]), int(self.jump_positions[0][1])), 2)
+            center = (int(self.jump_positions[0][0]), int(self.jump_positions[0][1]))
+            pygame.draw.circle(self.surface, pygame.Color(0, 0, 0), center, 2)
             self.jump_positions.pop(0)
         if keys[self.k_boost] and pygame.time.get_ticks() - self.last_boost_time >= self.boost_delay:
             self.last_boost_time = pygame.time.get_ticks()
